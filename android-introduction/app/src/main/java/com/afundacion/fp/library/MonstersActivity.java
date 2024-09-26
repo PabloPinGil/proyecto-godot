a8b42a4db765a804929cd5f2c22e9b3680967c90
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2024-09-26 10:34:58 +0200

    Tarea 15

--- android-introduction/app/src/main/java/com/afundacion/fp/library/MonstersActivity.java
+++ android-introduction/app/src/main/java/com/afundacion/fp/library/MonstersActivity.java
@@ -1,14 +1,41 @@
 package com.afundacion.fp.library;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
 public class MonstersActivity extends AppCompatActivity {
+    private Context context = this;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monsters);
+
+        BottomNavigationView bar = findViewById(R.id.bottomNavigation);
+        bar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.item1) {
+                    Toast.makeText(context, "Este es el primero", Toast.LENGTH_LONG).show();
+                }
+                if (item.getItemId() == R.id.item2) {
+                    Toast.makeText(context, "Segundo Digimon", Toast.LENGTH_LONG).show();
+                }
+                if (item.getItemId() == R.id.item3) {
+                    Toast.makeText(context, "Este es el tercer Digimon", Toast.LENGTH_LONG).show();
+                }
+
+                return true;
+            }
+        });
     }
 }
