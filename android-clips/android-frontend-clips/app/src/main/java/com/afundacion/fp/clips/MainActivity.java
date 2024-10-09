4a01d69f7247dd545bac62c8b73bee6117afd012
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2024-10-09 12:39:19 +0200

    tarea #23: una petición de una lista

--- android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/MainActivity.java
+++ android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/MainActivity.java
@@ -1,6 +1,8 @@
 package com.afundacion.fp.clips;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
 import android.os.Bundle;
 import android.widget.Toast;
 import android.content.Context;
@@ -9,22 +11,28 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.snackbar.Snackbar;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private RequestQueue queue;
-    private Context context;
+    private Context context = this;
+    private ConstraintLayout mainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.context = this;
         this.queue = Volley.newRequestQueue(context);
+        this.mainLayout = findViewById(R.id.main_layout);
+
+        requestClipList();
 
         JsonObjectRequest request = new JsonObjectRequest(
                 Request.Method.GET,
@@ -39,19 +47,40 @@ public class MainActivity extends AppCompatActivity {
                             throw new RuntimeException(e);
                         }
                     }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                if (error.networkResponse == null) {
-                    Toast.makeText(context, "Could not establish connection", Toast.LENGTH_SHORT).show();
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        if (error.networkResponse == null) {
+                            Toast.makeText(context, "Could not establish connection", Toast.LENGTH_SHORT).show();
 
-                } else {
-                    int serverCode = error.networkResponse.statusCode;
-                    Toast.makeText(context, "Server status: " + serverCode, Toast.LENGTH_SHORT).show();
+                        } else {
+                            int serverCode = error.networkResponse.statusCode;
+                            Toast.makeText(context, "Server status: " + serverCode, Toast.LENGTH_SHORT).show();
+                        }
                 }
-
-            }
         }
+        );
+        this.queue.add(request);
+    }
+
+    private void requestClipList() {
+        JsonArrayRequest request = new JsonArrayRequest(
+                Request.Method.GET,
+                Server.name + "/clips",
+                null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Snackbar.make(mainLayout, "List received", Snackbar.LENGTH_LONG).show();
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
         );
         this.queue.add(request);
     }
