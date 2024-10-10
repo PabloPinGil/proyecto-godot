9002186fd08757cf6dcd0976fcf4c5d68eea5c24
PabloPinGil <ppineirog23@fpcoruna.afundacion.org>
2024-10-10 09:55:50 +0200

    tarea #33: mostrando vídeos

--- android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/VideoActivity.java
+++ android-clips/android-frontend-clips/app/src/main/java/com/afundacion/fp/clips/VideoActivity.java
@@ -1,13 +1,17 @@
 package com.afundacion.fp.clips;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
 import android.os.Bundle;
 import android.content.Intent;
 import android.widget.Toast;
+import android.widget.VideoView;
 
 public class VideoActivity extends AppCompatActivity {
     public static final String INTENT_CLIP_ID = "CLIP_ID";
     public static final String INTENT_CLIP_URL = "CLIP_URL";
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +23,9 @@ public class VideoActivity extends AppCompatActivity {
         String clipUrl = intent.getStringExtra(VideoActivity.INTENT_CLIP_URL);
 
         Toast.makeText(this, "clipID: " + clipId, Toast.LENGTH_LONG).show();
+
+        videoView = findViewById(R.id.video_view);
+        videoView.setVideoURI(Uri.parse(clipUrl));
+        videoView.start();
     }
 }
